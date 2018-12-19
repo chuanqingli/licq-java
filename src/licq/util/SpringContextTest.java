f0cac7597135f8ec78d48edd0d184af0199095a3
chuanqingli <9915252@qq.com>
2018-12-19 17:46:06 +0800

    修改

--- src/licq/util/SpringContextTest.java
+++ src/licq/util/SpringContextTest.java
@@ -1,6 +1,9 @@
 package licq.util;
 import licq.impl.util.*;
+import java.io.*;
+import java.net.*;
 import java.util.*;
+import java.util.jar.*;
 
 
 import org.junit.Assert;
@@ -12,25 +15,19 @@ import org.junit.Ignore;
 
 public class SpringContextTest{
 
-  @Test
-    public void toDateTest0(){
-
-Class<?> interfaces[] = CollectionUtilImpl.class.getInterfaces();
-
-System.out.println("==>" + interfaces.length + ":" + Arrays.asList(interfaces)  + "::");
-      CollectionUtil util = ImplFactory.getBean(CollectionUtil.class);
+    @Test
+    public void dotest(){
+        DataUtil util = ImplFactory.getBean(DataUtil.class);
+        System.out.println(util.getClass().getName());
 
 
-      Collection<Integer> set = util.create(new LinkedHashSet<Integer>(),0,new Object[]{123,"123","345",345.6,895.6});
-      System.out.println("==>" + set + "::");
-
-
-      // DataUtil util = SpringContext.getBean(DataUtil.class);
-
-      // int nnn = util.toData("234.6",0);
-      // System.out.println("==>" + nnn + "::");
-      // DataUtil util2 = SpringContext.getBean(DataUtil.class);
-      // System.out.println("==>" + (util==util2) + "::");
     }
 
+    @Test
+    public void dotest1(){
+        DataUtil util = SpringContext.getBean(DataUtilImpl.class);
+        System.out.println(util.getClass().getName());
+
+
+    }
 }
